407b0d996a4fb00da866642651cc493dcdca4883
Casey Marshall <cmars@cmarstech.com>
2014-04-04 19:03:41 -0500

    Remove blurbage from the help doc, its on the project page.

--- main.go
+++ main.go
@@ -34,34 +34,6 @@ type NatCommand struct {
 var doc = `
 juju-nat sets up NAT routing to expose ports to service units deployed inside
 containers.
-
-Example:
-
-Given a service deployed to an LXC container:
-
- $ juju deploy wordpress --to lxc:0
- $ juju status wordpress
-
-machines:
-  "0":
-    dns-name: 192.168.122.107
-    containers:
-      0/lxc/2:
-        dns-name: 10.0.3.151
-        instance-id: juju-machine-0-lxc-2
-services:
-  owncloud:
-    charm: cs:precise/owncloud-12
-    exposed: true
-    units:
-      owncloud/0:
-        machine: 0/lxc/2
-        open-ports:
-        - 80/tcp
-        public-address: 10.0.3.151
-
-'juju nat' will expose port 80 on the containing machine (192.168.122.107), routed to
-port 80 on the container where the service is deployed (10.0.3.151).
 `
 
 type Forward struct {
